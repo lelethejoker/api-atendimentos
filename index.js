0fef467f85acd60278b4988cdb373c606a74458a
jhonatan daniel daniel <0daniel.lele@gmail.com>
2026-05-14 16:16:08 -0300

    docs: atualização README com correção de erros em arquitetura middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000;
 const router = require('./routers/router-index');
 const conexao = require('./infraestrutura/conexao');
 const tabelas = require('./infraestrutura/tabelas');
+
 app.use(express.json());
 tabelas.init(conexao);
 router(app);
@@ -15,4 +16,9 @@ app.listen(port, (err) => {
     } else {
         console.log(`Servidor esta rodando na porta ${port}`);
     }
+});
+
+app.use((err, req, res, next) => {
+    console.error('Erro:', err);
+    res.status(500).json({ error: 'Ocorreu um erro no servidor.' });
 });
